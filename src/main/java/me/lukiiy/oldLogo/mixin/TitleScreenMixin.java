da69eb882df0adf46ab919008eac7aeae07580cb
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-01-16 14:41:27 -0300

    Make it actually work?
    * More GL stuff and fixes

--- src/main/java/me/lukiiy/oldLogo/mixin/TitleScreenMixin.java
+++ src/main/java/me/lukiiy/oldLogo/mixin/TitleScreenMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.class_564;
 import net.minecraft.class_13;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.render.Tessellator;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
 import org.spongepowered.asm.mixin.Final;
@@ -65,12 +64,11 @@ public class TitleScreenMixin {
             logoEffects = new LogoEffectRandomizer[OLD_LOGO[0].length()][OLD_LOGO.length];
 
             for (int x = 0; x < logoEffects.length; x++) {
-                for (int y = 0; y < logoEffects[x].length; y++) {
-                    logoEffects[x][y] = new LogoEffectRandomizer(RANDOM, x, y);
-                }
+                for (int y = 0; y < logoEffects[x].length; y++) logoEffects[x][y] = new LogoEffectRandomizer(RANDOM, x, y);
             }
         }
 
+        GL11.glPushAttrib(GL11.GL_ENABLE_BIT | GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_TEXTURE_BIT);
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glPushMatrix();
         GL11.glLoadIdentity();
@@ -78,35 +76,34 @@ public class TitleScreenMixin {
         class_564 scaledRes = new class_564(minecraft.options, minecraft.displayWidth, minecraft.displayHeight);
         int viewportHeight = 120 * scaledRes.field_2391;
 
-        GLU.gluPerspective(70f, (float) minecraft.displayWidth / (float) viewportHeight, .05f, 100f);
+        GLU.gluPerspective(70, (float) minecraft.displayWidth / (float) viewportHeight, .05f, 100);
         GL11.glViewport(0, minecraft.displayHeight - viewportHeight, minecraft.displayWidth, viewportHeight);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
         GL11.glPushMatrix();
         GL11.glLoadIdentity();
+
         GL11.glDisable(GL11.GL_CULL_FACE);
         GL11.glCullFace(GL11.GL_BACK);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(true);
+        GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
 
         class_13 blockRenderer = new class_13();
 
         for (int pass = 0; pass < 3; pass++) {
             GL11.glPushMatrix();
-            GL11.glTranslatef(0.4F, 0.6F, -13.0F);
+            GL11.glTranslatef(.4f, .6f, -13f);
 
             if (pass == 0) {
                 GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
-                GL11.glTranslatef(0.0F, -0.4F, 0.0F);
-                GL11.glScalef(0.98F, 1.0F, 1.0F);
+                GL11.glTranslatef(0, -.4f, 0);
+                GL11.glScalef(.98f, 1, 1);
                 GL11.glEnable(GL11.GL_BLEND);
                 GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            }
-
-            if (pass == 1) {
+            } else if (pass == 1) {
                 GL11.glDisable(GL11.GL_BLEND);
                 GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
-            }
-
-            if (pass == 2) {
+            } else {
                 GL11.glEnable(GL11.GL_BLEND);
                 GL11.glBlendFunc(GL11.GL_SRC_COLOR, GL11.GL_ONE);
             }
@@ -115,8 +112,9 @@ public class TitleScreenMixin {
             GL11.glRotatef(15f, 1, 0, 0);
             GL11.glScalef(.89f, 1, .4f);
             GL11.glTranslatef(-OLD_LOGO[0].length() * .5f, -OLD_LOGO.length * .5f, 0);
-            GL11.glBindTexture(GL11.GL_TEXTURE_2D, minecraft.textureManager.getTextureId("/terrain.png"));
+
             if (pass == 0) GL11.glBindTexture(GL11.GL_TEXTURE_2D, minecraft.textureManager.getTextureId("/title/black.png"));
+            else GL11.glBindTexture(GL11.GL_TEXTURE_2D, minecraft.textureManager.getTextureId("/terrain.png"));
 
             for (int y = 0; y < OLD_LOGO.length; y++) {
                 for (int x = 0; x < OLD_LOGO[y].length(); x++) {
@@ -127,7 +125,6 @@ public class TitleScreenMixin {
 
                     float z = (float) (effect.current + (effect.target - effect.current) * delta);
                     float scale = 1;
-
                     if (pass == 0) {
                         scale = z * .04f + 1;
                         z = 0;
@@ -135,8 +132,9 @@ public class TitleScreenMixin {
 
                     GL11.glTranslatef(x, y, z);
                     GL11.glScalef(scale, scale, scale);
+                    GL11.glColor4f(1, 1, 1, 1);
 
-                    blockRenderer.method_48(Block.STONE, 16, .77f);
+                    blockRenderer.method_48(Block.STONE, 0, .77f);
 
                     GL11.glPopMatrix();
                 }
@@ -152,5 +150,6 @@ public class TitleScreenMixin {
         GL11.glPopMatrix();
         GL11.glViewport(0, 0, minecraft.displayWidth, minecraft.displayHeight);
         GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glPopAttrib();
     }
 }
